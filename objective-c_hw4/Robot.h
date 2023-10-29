8b214b969032d5701915098c41cd2cb5d587a458
dsail <dsail@yandex.ru>
2023-10-29 09:48:57 +0300

    commit

---
+++ objective-c_hw4/Robot.h
@@ -0,0 +1,16 @@
+//
+//  Robot.h
+//  objective-c_hw4
+//
+//  Created by Eugene Zvyagin on 29.10.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Robot : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ objective-c_hw4/Robot.m
@@ -0,0 +1,12 @@
+//
+//  Robot.m
+//  objective-c_hw4
+//
+//  Created by Eugene Zvyagin on 29.10.2023.
+//
+
+#import "Robot.h"
+
+@implementation Robot
+
+@end
